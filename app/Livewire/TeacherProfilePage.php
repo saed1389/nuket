6c9af5a90be07746217d12f4f2c0e540fb5cc6b6
saed1389 <saed.1389@gmail.com>
2024-07-26 18:28:11 +0300

    teacher pages

---
+++ app/Livewire/TeacherProfilePage.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class TeacherProfilePage extends Component
+{
+    public function render()
+    {
+        return view('livewire.teacher-profile-page');
+    }
+}
